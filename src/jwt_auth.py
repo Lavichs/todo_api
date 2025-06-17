0aadcb45eed10c2b550e81b15a4ee4f9de431496
Lavichs <barmalej54d132@gmail.com>
2025-06-18 00:02:09 +0500

    chore: fix jwt config

--- src/jwt_auth.py
+++ src/jwt_auth.py
@@ -3,7 +3,6 @@ from authx import AuthX, AuthXConfig, RequestToken
 from config import settings
 
 config = AuthXConfig(
-    JWT_SECRET_KEY="SECRET_KEY",
     JWT_TOKEN_LOCATION=["cookies"],
     JWT_ACCESS_COOKIE_NAME="my_access_token",
     JWT_CSRF_METHODS=[]
